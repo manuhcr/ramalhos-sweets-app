abb9af175a3d3522741b6dd79a2723862bd1551b
manuhzita <mitskiws@gmail.com>
2026-05-15 17:24:17 -0300

    muitas mudanças

--- telas/Home.tsx
+++ telas/Home.tsx
@@ -1,62 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView, TextInput, TouchableOpacity } from 'react-native';
-import { VideoView, useVideoPlayer } from 'expo-video';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useRoute } from '@react-navigation/native';
 import { FlatList } from 'react-native';
 import { useState } from 'react';
+import Svg, { Path } from 'react-native-svg';
 
 
 
 export default function Home() {
 
-    const [categoriaSelecionada, setCategoriaSelecionada] = useState(null)
+    type Categoria = keyof typeof cardapio | 'todos';
+    const [categoriaSelecionada, setCategoriaSelecionada] = useState<Categoria>('todos');
 
 
-    const player = useVideoPlayer(
-        "https://www.pexels.com/pt-br/download/video/8870910/",
-        player => {
-            player.loop = true;
-            player.play();
-        }
-    );
-    const categorias = [
-        {
-            id: "bolos",
-            img: require('../assets/pedaco-de-bolo.png'),
-        },
+    const dia = new Date().getDay();
 
-        {
-            id: "doces",
-            img: require('../assets/brigadeiro.png'),
-        },
-
-        {
-            id: "colddesserts",
-            img: require('../assets/sorvete.png'),
-        },
-
-        {
-            id: "chocoandbrownie",
-            img: require('../assets/chocolate.png'),
-        },
-
-        {
-            id: "especiais",
-            img: require('../assets/doces.png'),
-        }
+    const banners = [
+        require('../assets/cupons/domingo.png'),
+        require('../assets/cupons/segunda.png'),
+        require('../assets/cupons/terca.png'),
+        require('../assets/cupons/quarta.png'),
+        require('../assets/cupons/quinta.png'),
+        require('../assets/cupons/sexta.png'),
+        require('../assets/cupons/sabado.png'),
     ];
+
 
     const cardapio = {
         bolos: [
-            { nome: "Bolo de chocolate com cobertura cremosa", preco: "R$ 49,90 (1kg)" },
+            { nome: "Bolo de chocolate com cobertura cremosa", preco: "R$ 49,90 (1kg)", img: require("../assets/bolochoco.png") },
             { nome: "Bolo de morango com chantilly", preco: "R$ 54,90 (1kg)" },
             { nome: "Bolo de cenoura com calda de chocolate", preco: "R$ 44,90 (1kg)" },
             { nome: "Bolo Red Velvet com cream cheese", preco: "R$ 64,90 (1kg)" },
-            { nome: "Cheesecake de frutas vermelhas", preco: "R$ 69,90 (1kg)" },
-            { nome: "Torta holandesa", preco: "R$ 59,90 (1kg)" },
-            { nome: "Torta de limão com merengue", preco: "R$ 49,90 (1kg)" },
-            { nome: "Torta de maçã caramelizada", preco: "R$ 55,90 (1kg)" }
+            { nome: "Bolo de ninho com morango", preco: "R$ 59,90 (1kg)" },
+            { nome: "Bolo de limão siciliano", preco: "R$ 52,90 (1kg)" },
+            { nome: "Bolo prestígio", preco: "R$ 56,90 (1kg)", img: require("../assets/boloprestigio.png") },
+            { nome: "Bolo de doce de leite", preco: "R$ 58,90 (1kg)" },
+            { nome: "Chessecake de frutas vermelhas", preco: "R$ 54,90 (1kg)" }
         ],
 
         doces: [
@@ -95,17 +75,81 @@ export default function Home() {
             { nome: "Donuts", preco: "R$ 8,90 (unidade)" }
         ]
     };
+    const [pesquisa, setPesquisa] = useState('');
+
+    const allItems = [
+        ...cardapio.bolos,
+        ...cardapio.doces,
+        ...cardapio.colddesserts,
+        ...cardapio.chocoandbrownie,
+        ...cardapio.especiais,
+    ];
+
+    const filtros = allItems.filter(item =>
+        item.nome.toLowerCase().includes(pesquisa.toLowerCase())
+    );
+    const categorias: {
+        id: Categoria;
+        img: any;
+    }[] = [
+            {
+                id: "bolos",
+                img: require('../assets/pedaco-de-bolo.png'),
+            },
+
+            {
+                id: "doces",
+                img: require('../assets/pedaco-de-bolo.png'),
+            },
+
+            {
+                id: "colddesserts",
+                img: require('../assets/sobremesasgeladas.webp'),
+            },
+
+            {
+                id: "chocoandbrownie",
+                img: require('../assets/chocolate.png'),
+            },
+
+            {
+                id: "especiais",
+                img: require('../assets/especiais.png'),
+            }
+        ];
+
     const destaques = categorias.map((categoria) => ({
         ...categoria,
-        quantidade: cardapio[categoria.id].length
+        quantidade: categoria.id === 'todos' ? allItems.length : cardapio[categoria.id].length
     }));
+
+    const [isSearchFocused, setIsSearchFocused] = useState(false);
+
     return (
 
+
         <LinearGradient
-            colors={['#FFF5F7', '#FFE4E8']}
+            colors={['#d88f8c6c', '#FFF5F7', '#ffd8d8']}
             style={{ flex: 1 }}
         >
 
+            <View style={styles.caldaContainer}>
+
+                <View style={styles.caldaBackground} />
+
+                <Svg
+                    width="100%"
+                    height={160}
+                    viewBox="0 0 1440 320"
+                    style={styles.svg}
+                >
+                    <Path
+                        fill="#fdd5d5"
+                        d="M0,288L30,266.7C60,245,120,203,180,202.7C240,203,300,245,360,266.7C420,288,480,288,540,266.7C600,245,660,203,720,208C780,213,840,267,900,288C960,309,1020,299,1080,277.3C1140,256,1200,224,1260,224C1320,224,1380,256,1410,272L1440,288L1440,0L0,0Z"
+                    />
+                </Svg>
+
+            </View>
 
             <ScrollView contentContainerStyle={styles.content}>
 
@@ -114,52 +158,87 @@ export default function Home() {
                 </View>
 
                 <View style={styles.search}>
-                     <TextInput placeholder=' Buscar doces...' style={{ color: '#999' }}></TextInput>
+                    <TextInput
+                        placeholder=" Buscar doces..."
+                        placeholderTextColor="#C89A9A"
+                        style={{
+                            color: '#5A3E36',
+                            backgroundColor: '#f1dede63',
+                            borderRadius: 8,
+                            fontSize: 15,
+                            flex: 1,
+
+                        }}
+                        onFocus={() => setIsSearchFocused(true)}
+                        onBlur={() => setIsSearchFocused(false)}
+                        value={pesquisa}
+                        onChangeText={setPesquisa}
+
+
+
+                    />
+
                 </View>
 
-                <Text style={styles.section}>Categorias</Text>
-
-                <View style={styles.categories}>
-                    {categorias.map((item) => (
-                        <TouchableOpacity
-                            key={item.id}
-                            style={styles.cat}
-                            onPress={() => setCategoriaSelecionada(item.id)}
-                        >
-                            <Image
-                                source={item.img}
-                                style={{ width: 20, height: 20 }}
-                            />
-                        
-                        </TouchableOpacity>
-
-                    ))}
+                <View style={styles.bannerContainer}>
+                    <Image
+                        source={banners[dia]}
+                        style={styles.banner}
+                        resizeMode="cover"
+                    />
                 </View>
-                <Text>{categoriaSelecionada}</Text>
-                {categoriaSelecionada && (
-                    <FlatList
-                        data={cardapio[categoriaSelecionada]}
-                        horizontal
-                        showsHorizontalScrollIndicator={false}
-                        keyExtractor={(item) => item.id}
-                        renderItem={({ item }) => (
-                            <View style={[styles.product, { width: 180 }]}>
+
+                <View style={{ marginTop: 60 }}>
+                    <Text style={styles.section}>Categorias</Text>
+                    <View style={styles.categories}>
+                        {categorias.map((item) => (
+                            <TouchableOpacity
+                                key={item.id}
+                                style={[
+                                    styles.cat,
+                                    categoriaSelecionada === item.id && styles.catSelected
+                                ]}
+                                onPress={() => setCategoriaSelecionada(item.id)}
+                            >
+                                <Image
+                                    source={item.img}
+                                    style={styles.catImg}
+                                />
+
+                            </TouchableOpacity>
+
+                        ))}
+                    </View>
+                </View>
+
+
+
+                <FlatList
+                    data={pesquisa ? filtros : categoriaSelecionada === 'todos' ? allItems : cardapio[categoriaSelecionada]}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <View style={[styles.product, { width: 220, marginRight: 15 }]}>
+
+                            {item.img && (
                                 <Image
                                     source={item.img}
                                     style={styles.productImg}
                                 />
+                            )}
 
-                                <Text style={styles.productName}>
-                                    {item.titulo}
-                                </Text>
+                            <Text style={styles.productName}>
+                                {item.nome}
+                            </Text>
 
-                                <Text style={styles.price}>
-                                    {item.quantidade} opções
-                                </Text>
-                            </View>
-                        )}
-                    />
-                )}
+                            <Text style={styles.price}>
+                                {item.preco}
+                            </Text>
+
+                        </View>
+                    )}
+                />
 
                 <Text style={styles.section}>Destaques</Text>
 
@@ -192,18 +271,35 @@ const styles = StyleSheet.create({
 
     content: {
         padding: 20,
-
+        paddingTop: 8
+    },
+    caldaContainer: {
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        height: 465,
     },
 
+    caldaBackground: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: '#fdd5d5',
+        height: 350,
+    },
+
+    svg: {
+        position: 'absolute',
+        bottom: 0,
+    },
     header: {
-        marginTop: 40,
+        marginTop: 50,
         marginBottom: 20,
+        zIndex: 2,
     },
 
     greeting: {
         fontSize: 30,
         color: '#A25F3C',
-        fontFamily: 'FonteRegular',
+        fontFamily: 'MisteryRegular',
     },
 
     title: {
@@ -214,35 +310,67 @@ const styles = StyleSheet.create({
 
     search: {
         backgroundColor: '#fff',
-        padding: 12,
+        padding: 6,
+        borderWidth: 0.5,
+        borderColor: '#5c2e16',
         borderRadius: 20,
         marginBottom: 20,
         display: 'flex',
         flexDirection: 'row'
     },
+    searchFocused: {
+        borderColor: '#bd4749',
+        borderWidth: 1,
+    },
+
+    bannerContainer: {
+        alignItems: 'center',
+    },
+
+    banner: {
+        width: '100%',
+        height: undefined,
+        aspectRatio: 2.6,
+        borderRadius: 18,
+    },
 
     section: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 10,
-        color: '#5A3E36'
+        fontSize: 21,
+        fontWeight: '700',
+        marginBottom: 14,
+        marginTop: 10,
+        color: '#5e382e',
+        fontFamily: 'MisteryRegular',
     },
 
     categories: {
-        flexDirection: 'row',
         justifyContent: 'space-between',
         marginBottom: 20,
+        display: 'flex',
+        flexDirection: 'row',
+
     },
 
     cat: {
-        backgroundColor: '#fff5e8',
         borderColor: '#A66C4D',
         borderWidth: 1,
-        padding: 20,
-        borderRadius: 100,
+        padding: 5,
+        borderRadius: 20,
         alignItems: 'center',
         width: 60,
         height: 60
+    },
+
+    catSelected: {
+        backgroundColor: '#D67274',
+        borderColor: '#D67274',
+    },
+
+    catImg: {
+        width: 50,
+        height: 50,
+        resizeMode: 'cover',
+        borderRadius: 15,
     },
 
     cards: {
@@ -250,22 +378,36 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
     },
 
+
     product: {
-        backgroundColor: '#fff',
-        borderRadius: 15,
-        padding: 10,
-        width: '40%',
+        backgroundColor: '#FFF9F7',
+        borderRadius: 20,
+        padding: 18,
+
+        shadowColor: '#000',
+
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+
+        shadowOpacity: 0.08,
+        shadowRadius: 8,
+
+        elevation: 5,
     },
 
     productImg: {
         width: '100%',
-        height: 60,
+        height: 40,
         borderRadius: 10,
     },
 
     productName: {
         marginTop: 8,
-        fontWeight: 'bold',
+        fontWeight: '600',
+        color: '#5A3E36',
+        fontSize: 15,
     },
 
     price: {
